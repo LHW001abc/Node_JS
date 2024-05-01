0a61d1f7775920e7aa659c9b0daf8e0e97d8c153
abdo <abdelmajidzadi@gmail.com>
2024-05-01 12:23:21 +0200

    this for my porject class

---
+++ Node-JS-Mongo-API/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://admin:example@products.0zociic.mongodb.net/?retryWrites=true&w=majority";
+const connectDB = async () => {
+    try {
+        await mongoose.connect(uri , 
+        {useNewUrlParser: true, 
+        useUnifiedTopology: true
+    });
+    console.log("MongoDB Connected")
+    }
+    catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    } 
+}
+
+module.exports = connectDB;
